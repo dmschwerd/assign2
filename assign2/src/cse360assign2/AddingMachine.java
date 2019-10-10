65187048e657c82713f27562c148166ebe3c24a5
David <David@DESKTOP-RKJ6694>
2019-10-09 20:33:19 -0700

    Second commitment

--- assign2/src/cse360assign2/AddingMachine.java
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,36 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String allOps = "0";
 	
 	public AddingMachine () {
 		total = 0;
 		// not needed - included for clarity
 		}
-		
+	
+	//gets the total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
+	
+	//adds to the total
 	public void add (int value) {
-			
+		total = total + value;
+		allOps = allOps + " + " + value;
 	}
-		
+	
+	//subtracts from the total
 	public void subtract (int value) {
-			
+		total = total - value;
+		allOps = allOps + " - " + value;
 	}
-		
+	
+	//returns the string allOps that shows all functions done to total
 	public String toString () {
-		return "";
+		return allOps;
 	}
 		
 	public void clear() {
-			
+		allOps = "0";
 	}
 }
